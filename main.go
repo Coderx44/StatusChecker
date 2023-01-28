1d5a03b8201e31a53c990716938fb551bad614a9
Prasad <bhosaleprasad849@gmail.com>
2023-01-28 23:38:08 +0530

    Add function checkWebsites to monitor status

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"net/http"
+	"time"
 
 	"github.com/Coderx44/StatusChecker/service"
 )
@@ -14,11 +15,43 @@ type StatusChecker interface {
 type httpChecker struct {
 }
 
+func (h httpChecker) Check(ctx context.Context, name string) (status bool, err error) {
+
+	res, err := http.Get("http://" + name)
+	if err != nil {
+		status = false
+		return
+	}
+
+	if res.StatusCode == 200 {
+		status = true
+	} else {
+		status = false
+	}
+	return
+}
+
+func checkWebsites(checkHttp *httpChecker) {
+	for {
+		for url := range service.WebsiteList {
+			status, err := checkHttp.Check(context.Background(), url)
+
+			if err != nil || !status {
+				service.WebsiteList[url] = "DOWN"
+			} else if status {
+				service.WebsiteList[url] = "UP"
+			}
+		}
+
+		time.Sleep(time.Minute)
+	}
+}
+
 func main() {
-	// checkHttp := httpChecker{}
+	checkHttp := httpChecker{}
 
 	http.HandleFunc("/website", service.HandleWebsites)
 
-	// go checkWebsites(&checkHttp)
+	go checkWebsites(&checkHttp)
 
 }
